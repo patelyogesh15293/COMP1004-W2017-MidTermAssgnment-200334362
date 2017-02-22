edd62a45000bdc7fb115e5d32b7f0cc67538ea22
yogesh patel <yopatel56@gmail.com>
2017-02-22 16:21:12 -0500

    Add functionality to Race Form

--- COMP1004-W2017-MidTermAssgnment-200334362/RaceForm.Designer.cs
+++ COMP1004-W2017-MidTermAssgnment-200334362/RaceForm.Designer.cs
@@ -32,13 +32,19 @@
             this.NextButton = new System.Windows.Forms.Button();
             this.RacialBonusLabel = new System.Windows.Forms.Label();
             this.RacialBonusTextBox = new System.Windows.Forms.TextBox();
+            this.CharacterRaceGroupBox = new System.Windows.Forms.GroupBox();
+            this.DwarfRadioButton = new System.Windows.Forms.RadioButton();
+            this.ELFRadioButton = new System.Windows.Forms.RadioButton();
+            this.HalflingRadioButton = new System.Windows.Forms.RadioButton();
+            this.HumanRadioButton = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.CharacterPictureBox)).BeginInit();
+            this.CharacterRaceGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // CharacterPictureBox
             // 
             this.CharacterPictureBox.Location = new System.Drawing.Point(239, 84);
-            this.CharacterPictureBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.CharacterPictureBox.Margin = new System.Windows.Forms.Padding(4);
             this.CharacterPictureBox.Name = "CharacterPictureBox";
             this.CharacterPictureBox.Size = new System.Drawing.Size(290, 262);
             this.CharacterPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
@@ -48,7 +54,7 @@
             // NextButton
             // 
             this.NextButton.Location = new System.Drawing.Point(525, 465);
-            this.NextButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.NextButton.Margin = new System.Windows.Forms.Padding(4);
             this.NextButton.Name = "NextButton";
             this.NextButton.Size = new System.Drawing.Size(100, 28);
             this.NextButton.TabIndex = 4;
@@ -61,7 +67,7 @@
             this.RacialBonusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.RacialBonusLabel.Location = new System.Drawing.Point(236, 366);
             this.RacialBonusLabel.Name = "RacialBonusLabel";
-            this.RacialBonusLabel.Size = new System.Drawing.Size(138, 25);
+            this.RacialBonusLabel.Size = new System.Drawing.Size(115, 20);
             this.RacialBonusLabel.TabIndex = 6;
             this.RacialBonusLabel.Text = "Racial Bonus";
             // 
@@ -70,22 +76,90 @@
             this.RacialBonusTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.RacialBonusTextBox.Location = new System.Drawing.Point(242, 400);
             this.RacialBonusTextBox.Name = "RacialBonusTextBox";
-            this.RacialBonusTextBox.Size = new System.Drawing.Size(287, 34);
+            this.RacialBonusTextBox.Size = new System.Drawing.Size(287, 29);
             this.RacialBonusTextBox.TabIndex = 7;
+            // 
+            // CharacterRaceGroupBox
+            // 
+            this.CharacterRaceGroupBox.Controls.Add(this.DwarfRadioButton);
+            this.CharacterRaceGroupBox.Controls.Add(this.ELFRadioButton);
+            this.CharacterRaceGroupBox.Controls.Add(this.HalflingRadioButton);
+            this.CharacterRaceGroupBox.Controls.Add(this.HumanRadioButton);
+            this.CharacterRaceGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CharacterRaceGroupBox.Location = new System.Drawing.Point(12, 84);
+            this.CharacterRaceGroupBox.Name = "CharacterRaceGroupBox";
+            this.CharacterRaceGroupBox.Size = new System.Drawing.Size(173, 262);
+            this.CharacterRaceGroupBox.TabIndex = 8;
+            this.CharacterRaceGroupBox.TabStop = false;
+            this.CharacterRaceGroupBox.Text = "Character\'s Race";
+            // 
+            // DwarfRadioButton
+            // 
+            this.DwarfRadioButton.AutoSize = true;
+            this.DwarfRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DwarfRadioButton.Location = new System.Drawing.Point(6, 79);
+            this.DwarfRadioButton.Name = "DwarfRadioButton";
+            this.DwarfRadioButton.Size = new System.Drawing.Size(65, 22);
+            this.DwarfRadioButton.TabIndex = 12;
+            this.DwarfRadioButton.TabStop = true;
+            this.DwarfRadioButton.Text = "Dwarf";
+            this.DwarfRadioButton.UseVisualStyleBackColor = true;
+            this.DwarfRadioButton.CheckedChanged += new System.EventHandler(this.DwarfRadioButton_CheckedChanged);
+            // 
+            // ELFRadioButton
+            // 
+            this.ELFRadioButton.AutoSize = true;
+            this.ELFRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ELFRadioButton.Location = new System.Drawing.Point(6, 123);
+            this.ELFRadioButton.Name = "ELFRadioButton";
+            this.ELFRadioButton.Size = new System.Drawing.Size(53, 22);
+            this.ELFRadioButton.TabIndex = 11;
+            this.ELFRadioButton.TabStop = true;
+            this.ELFRadioButton.Text = "ELF";
+            this.ELFRadioButton.UseVisualStyleBackColor = true;
+            this.ELFRadioButton.CheckedChanged += new System.EventHandler(this.ELFRadioButton_CheckedChanged);
+            // 
+            // HalflingRadioButton
+            // 
+            this.HalflingRadioButton.AutoSize = true;
+            this.HalflingRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HalflingRadioButton.Location = new System.Drawing.Point(6, 171);
+            this.HalflingRadioButton.Name = "HalflingRadioButton";
+            this.HalflingRadioButton.Size = new System.Drawing.Size(74, 22);
+            this.HalflingRadioButton.TabIndex = 10;
+            this.HalflingRadioButton.TabStop = true;
+            this.HalflingRadioButton.Text = "Halfling";
+            this.HalflingRadioButton.UseVisualStyleBackColor = true;
+            this.HalflingRadioButton.CheckedChanged += new System.EventHandler(this.HalflingRadioButton_CheckedChanged);
+            // 
+            // HumanRadioButton
+            // 
+            this.HumanRadioButton.AutoSize = true;
+            this.HumanRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HumanRadioButton.Location = new System.Drawing.Point(6, 32);
+            this.HumanRadioButton.Name = "HumanRadioButton";
+            this.HumanRadioButton.Size = new System.Drawing.Size(74, 22);
+            this.HumanRadioButton.TabIndex = 9;
+            this.HumanRadioButton.TabStop = true;
+            this.HumanRadioButton.Text = "Human";
+            this.HumanRadioButton.UseVisualStyleBackColor = true;
             // 
             // RaceForm
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(641, 508);
+            this.Controls.Add(this.CharacterRaceGroupBox);
             this.Controls.Add(this.RacialBonusTextBox);
             this.Controls.Add(this.RacialBonusLabel);
             this.Controls.Add(this.CharacterPictureBox);
             this.Controls.Add(this.NextButton);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "RaceForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Select Character Race";
             ((System.ComponentModel.ISupportInitialize)(this.CharacterPictureBox)).EndInit();
+            this.CharacterRaceGroupBox.ResumeLayout(false);
+            this.CharacterRaceGroupBox.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -97,5 +171,10 @@
         internal System.Windows.Forms.Button NextButton;
         private System.Windows.Forms.Label RacialBonusLabel;
         private System.Windows.Forms.TextBox RacialBonusTextBox;
+        private System.Windows.Forms.GroupBox CharacterRaceGroupBox;
+        private System.Windows.Forms.RadioButton DwarfRadioButton;
+        private System.Windows.Forms.RadioButton ELFRadioButton;
+        private System.Windows.Forms.RadioButton HalflingRadioButton;
+        private System.Windows.Forms.RadioButton HumanRadioButton;
     }
 }
--- COMP1004-W2017-MidTermAssgnment-200334362/RaceForm.cs
+++ COMP1004-W2017-MidTermAssgnment-200334362/RaceForm.cs
@@ -21,5 +21,47 @@ namespace COMP1004_W2017_MidTermAssgnment_200334362
         }
 
         public AbilityForm PreviousForm = new AbilityForm();
+
+        /// <summary>
+        /// Create a method for chaange picture to pictubox when use select different radio button
+        /// </summary>
+        private void _pictureSelection()
+        {
+            if (HalflingRadioButton.Checked == true)
+            {
+                CharacterPictureBox.Load("c:/users/yopat/documents/visual studio 2015/Projects/COMP1004-W2017-MidTermAssgnment-200334362/COMP1004-W2017-MidTermAssgnment-200334362/Resources/M_Halfling2.png");
+            }
+            else if (HumanRadioButton.Checked == true)
+            {
+                CharacterPictureBox.Load("c:/users/yopat/documents/visual studio 2015/Projects/COMP1004-W2017-MidTermAssgnment-200334362/COMP1004-W2017-MidTermAssgnment-200334362/Resources/M_Human1.png");
+            }
+            else if (ELFRadioButton.Checked == true)
+            {
+                CharacterPictureBox.Load("c:/users/yopat/documents/visual studio 2015/Projects/COMP1004-W2017-MidTermAssgnment-200334362/COMP1004-W2017-MidTermAssgnment-200334362/Resources/M_Elf1.png");
+            }
+            else if (DwarfRadioButton.Checked == true)
+            {
+                CharacterPictureBox.Load("c:/users/yopat/documents/visual studio 2015/Projects/COMP1004-W2017-MidTermAssgnment-200334362/COMP1004-W2017-MidTermAssgnment-200334362/Resources/M_Dwarf1.png");
+            }
+            else
+            {
+                CharacterPictureBox.Load("c:/users/yopat/documents/visual studio 2015/Projects/COMP1004-W2017-MidTermAssgnment-200334362/COMP1004-W2017-MidTermAssgnment-200334362/Resources/M_Halfling2.png");
+            }
+        }
+
+        private void HalflingRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            _pictureSelection();
+        }
+
+        private void DwarfRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            _pictureSelection();
+        }
+
+        private void ELFRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            _pictureSelection();
+        }
     }
 }
